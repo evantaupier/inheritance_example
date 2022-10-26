057220b3d9d03522eb4f7a87b4183126d5d10eb5
EvanTaupier <drummertitan@gmail.com>
2022-10-26 14:15:52 -0400

    vehicles

--- employee.rb
+++ employee.rb
@@ -1,27 +1,12 @@
-# represents emplyee data as an array
-
-# employee1 = ["Majora", "Carter", 80000, true]
-# employee2 = ["Danilo", "Campos", 70000, true]
-
-# puts employee1[0] + " " + employee1[1] + " makes " + employee1[2].to_s + " a year."
-# puts "#{employee2[0]} #{employee2[1]} makes #{employee2[2]} a year."
-
-# represent data employee as a hash
-
-# employee1 = { :first_name => "Majora", :last_name => "Carter", :salary => 80000, :active => true }
-# employee2 = { first_name: "Danilo", last_name: "Campos", salary: 70000, active: true }
-
-# puts "#{employee1[:first_name]} #{employee1[:last_name]} makes #{employee1[:salary]} a year."
-# puts "#{employee2[:first_name]} #{employee2[:last_name]} makes #{employee2[:salary]} a year."
-
-# represents emplyees as a class instance
-
 class Employee
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
+  attr_reader :first_name, :last_name, :active
+  attr_writer :active
+
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def print_info
@@ -31,24 +16,27 @@ class Employee
   def give_annual_raise
     @salary = 1.05 * @salary
   end
+end
 
-  def first_name
-    @first_name
+employee1 = Employee.new({ first_name: "Majora", last_name: "Carter", salary: 80000, active: true })
+employee2 = Employee.new({ first_name: "Damilo", last_name: "Campos", salary: 70000, active: true })
+
+class Manager < Employee
+  attr_reader :employees
+
+  def initialize(input_options)
+    super
+    @employees = input_options[:employees]
   end
 
-  def last_name
-    @last_name
-  end
-
-  def active
-    @active
+  def send_report
+    puts "sending emial..."
+    # use email sending library
+    puts "email sent!"
   end
 end
 
-employee1 = Employee.new("Majora", "Carter", 80000, true)
-employee2 = Employee.new("Damilo", "Campos", 70000, true)
-employee1.print_info
-employee2.print_info
-employee2.give_annual_raise
-employee2.print_info
-p employee1.first_name
+manager = Manager.new(first_name: "Sharon", last_name: "Jacobs", salary: 100000, active: true, employees: [employee1, employee2])
+manager.print_info
+manager.send_report
+pp manager.employees
---
+++ inheritance_example.rb
@@ -0,0 +1,41 @@
+class Vehicle
+  def initialize
+    @speed = 0
+    @direction = "north"
+  end
+
+  def brake
+    @speed = 0
+  end
+
+  def accelerate
+    @speed += 10
+  end
+
+  def turn(new_direction)
+    @direction = new_direction
+  end
+end
+
+class Car < Vehicle
+  def initialize
+    @make = "Honda"
+    @model = "Civic"
+    @weight = 25
+  end
+
+  def honk_horn
+    puts "Beeeeeeep!"
+  end
+end
+
+class Bike < Vehicle
+  def initialize
+    @type = "Mountain"
+    @weight = 10
+  end
+
+  def ring_bell
+    puts "Ring ring!"
+  end
+end
